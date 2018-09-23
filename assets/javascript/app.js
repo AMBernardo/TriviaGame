d0fedf25a4f36b2403214e4fe9bf95d1ef3b1e1f
Retr0 <Ghozt24@gmail.com>
2018-09-22 21:55:35 -0700

    clean up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-    //Creating variable to track the question & "slide" numbers
+    
     var questionCounter = 0;
     
-    // timeout 
+   
     var ansTimeout = 2000;
     
-    //Creating score variables
+    
     var correct = 0;
     var incorrect = 0;
     var missed = 0;
     
-    //Creating array of user's answers
+    
     var userAns = [];
     
-    //Creating an array of objects with the questions, answer options, and correct answer
+    
     var questions = [
     {
         question: "What is the name of the Nissan R34 Skyline engine?",
@@ -36,13 +36,13 @@ $(document).ready(function() {
         choicesAnswer: 3
     }];
     
-    //Function to submit answers
+    
     function submitAns() {
         $("#submit").on("click", function(e) {
             e.preventDefault();
             userAns.length = 0;
                 
-            //Record user answer to question
+            
             var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
             userAns.push(userSelection);
             console.log(userAns);
@@ -50,7 +50,7 @@ $(document).ready(function() {
         });
     };
         
-    //Creating question timer variables & functions
+    
     var timeLeft = 10;
     var increment;
     
@@ -64,7 +64,7 @@ $(document).ready(function() {
         if (timeLeft === 0) {
             stopTimer();
             userAns.length = 0;		
-            //Record user answer to question
+            
             var userSelection = $("#responses input:radio[name=optionsRadios]:checked").val();
             userAns.push(userSelection);
             console.log(userAns);
@@ -85,10 +85,10 @@ $(document).ready(function() {
         clearInterval(increment);
     };
     
-    //Function to display the given response options
+    
     function createRadios() {
         var responseOptions = $("#responses");
-        //Empty array for user answer
+        
         responseOptions.empty();
             
         for (var i = 0; i < questions[questionCounter].choices.length; i++) {
@@ -96,32 +96,32 @@ $(document).ready(function() {
         };
     };
     
-    //Function to display the given question
+    
     function displayQ() {
         clearQ();
         resetTimer();
         $(".questionX").html(questions[questionCounter].question);
-        //Calling the function to display the response options
+        
         createRadios();
-        //Creating submit button
+        
         $("#submit-div").append('<button type="submit" class="btn btn-default" id="submit">' + "Submit" + '</button>');
         runTimer()
         submitAns();
     };
     
-    //Display start page
+    
     function displayStart() {
         $("#content").append('<a href="#" class="btn btn-primary btn-lg" id="start-button">' + "Start" + '</a>');
-        //Start game
+        
         $("#start-button").on("click", function(event) {
             event.preventDefault();
-            //Displays the first question
+            
             firstQ();
             resetTimer();
         });
     };
     
-    //Reset for end of game
+    
     function reset() {
         questionCounter = 0;
         correct = 0;
@@ -131,21 +131,21 @@ $(document).ready(function() {
         resetTimer();
     };
     
-    //Display end page
+    
     function displayEnd() {
         clearQ();
         $("#content").append('<h3>' + "Correct answers: " + correct + '</h3><br><h3>' + "Incorrect answers: " + incorrect + '</h3><br><h3>' + "Skipped questions: " + missed + '</h3><br><br><a href="#" class="btn btn-primary btn-lg" id="restart-button">' + "Restart Game" + '</a>');
         //Restart game
         $("#restart-button").on("click", function(event) {
             event.preventDefault();
-            //Displays the first question
+            
             reset();
             clearQ();
             displayStart();
         });
     };
     
-    //Function to clear the question
+    
     function clearQ() {
         var questionDiv = $(".questionX");
         questionDiv.empty();
@@ -162,7 +162,7 @@ $(document).ready(function() {
         stopTimer();
     };
     
-    //Showing whether answer was right/wrong
+    
     function checkQ() {
         clearQ();
         var correctAnswer = questions[questionCounter].choicesAnswer;
@@ -183,12 +183,12 @@ $(document).ready(function() {
         };
     };
     
-    //Function to change the question 
+    
     function nextQ() {
         checkQ();
-        //Incrementing the count by 1
+        
         questionCounter++;
-        //If the count is the same as the length of the question array, the counts reset to 0
+        
         if (questionCounter === questions.length) {
             setTimeout(displayEnd, ansTimeout);
         } 
@@ -197,14 +197,14 @@ $(document).ready(function() {
         };
     };
     
-    //Function to call the first question
+    
     function firstQ() {
         var startContent = $("#content");
         startContent.empty(); 
         displayQ();
     };
     
-    //Displays the start page
+    
     displayStart();
     
     });
